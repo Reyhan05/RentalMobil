9d9ae7970ebbc0d82a7975bbbb3bd799d1b3f761
Reyhan05 <kingreyhan298@gmail.com>
2023-05-29 10:33:33 +0700

    delete class tailwind in h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="container mx-auto px-4">
-    <h1 class="text-3xl font-semibold text-gray-800 mb-6">Rental Mobil</h1>
+    <h1>Rental Mobil</h1>
   
   </div>
 @endsection
