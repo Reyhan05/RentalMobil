dee46ac3b9f83a334156fa738383bb8de6c1b547
Reyhan05 <kingreyhan298@gmail.com>
2023-06-07 11:04:37 +0700

    feat: Create button modal in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container mx-auto px-4">
     <h1>Rental Mobil</h1>
     <div class="row row-cols-1 row-cols-md-4 g-4">
-      @foreach ($mobil as $mbl)
+        @foreach ($mobil as $mbl)
         <div class="col">
             <div class="card h-100">
                 <img src="..." class="card-img-top" alt="...">
@@ -13,16 +13,14 @@
                     <p class="card-text">{{ $mbl->merk->nama_merk }}</p>
                     <small class="text-muted">Rp.{{ $mbl->harga_sewa }},00</small>
                 </div>
-              
+
                 <div class="card-footer">
-                    <a href="" class="btn btn-dark btn-primary btn-sm" id="button-toggle">
-                      Sewa
-                      <i class="bx  bxs-chevrons-right" id="icon"></i>
-                    </a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Sewa
+                    </button>
                 </div>
             </div>
         </div>
-
         <div class="col">
             <div class="card h-100">
                 <img src="..." class="card-img-top" alt="...">
@@ -31,16 +29,34 @@
                     <p class="card-text">{{ $mbl->merk->nama_merk }}</p>
                     <small class="text-muted">Rp.{{ $mbl->harga_sewa }},00</small>
                 </div>
-              
+
                 <div class="card-footer">
-                    <a href="" class="btn btn-dark btn-primary btn-sm" id="button-toggle">
-                      Sewa
-                      <i class="bx  bxs-chevrons-right" id="icon"></i>
-                    </a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Sewa
+                    </button>
                 </div>
             </div>
         </div>
         @endforeach
     </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
 @endsection
