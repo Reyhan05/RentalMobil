38eca92e1efc686ae7066e312fa524ec558e91cd
Reyhan05 <kingreyhan298@gmail.com>
2023-06-07 14:44:45 +0700

    feat: add sweetalert when sign up

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -45,24 +45,15 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                <form class="nav-link text-white" action="{{ route('logout') }}" method="POST" id="signup-form">
+                    @csrf
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="bx bxs-log-out opacity-10"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Sign Up</span>
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
+                    <button class="nav-link-text ms-1" >Sign Up</button>
                 </form>
-
             </li>
         </ul>
     </div>
-    <!-- <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-      <div class="mx-3">
-        <a class="btn bg-gradient-primary mt-4 w-100" href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
-      </div>
-    </div> -->
 </aside>
+@extends('layouts.js')
